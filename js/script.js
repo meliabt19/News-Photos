22e4c6523b622745cf1a3def300ba39c8621a4c7
Nick Romano <njr7romano@yahoo.com>
2020-03-28 13:55:18 -0400

    initial files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const NEWS_API_KEY = 'a75938fbed57442696e756c8b556a3e8';
+
+const PLACES_API_KEY = '';
+
+const queryURL = 'https://api.sygictravelapi.com/1.2/en?x-api-key=' + PLACES_API_KEY;
+
+$(document).ready(function() {
+
+    /*
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            success: sendResponse,
+            error: function() {
+                //ajax call failed:
+                alert('Error!');
+            }
+        });     
+
+        function sendResponse(response) {
+            console.log(response);
+        }
+    */
+    
+});
